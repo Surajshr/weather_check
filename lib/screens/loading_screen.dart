8833c45af7f1a2ff3941e86a1a599df1e13598be
Surajshr <suraj575shrestha@gmail.com>
2022-04-06 07:35:46 +0545

    connected to the API and decoded that data.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/location.dart';
+
+const apiKey = "00f2f5b85d1ad2d8b1a081ffc873199b";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,23 +13,59 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    location newLocation = location();
+    await newLocation.getCurrentLocation();
+    latitude = newLocation.latitude;
+    longitude = newLocation.longitude;
+    getData();
+  }
+
+  void getData() async {
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      double cityTemprature = decodedData['main']['temp'];
+      int weatherCondition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(cityTemprature);
+      print(weatherCondition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    String myMargin = '15';
+    double? myMarginAsADouble;
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsADouble ?? 45),
+        color: Colors.red,
       ),
     );
   }
